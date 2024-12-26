6c0b9a97bb2addd8c32430a9ff3082673d3bae1c
azerkazimov <648azer@gmail.com>
2024-12-26 21:32:05 +0400

    update: folder structure, routing first steps

---
+++ src/app/about/page.tsx
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+export default function About() {
+  return (
+    <main className="flex flex-col items-center justify-center w-full min-h-screen gap-3">
+      <h1>About page</h1>
+      <Link href="/">
+        <button className="px-5 py-2 border border-black rounded-md   ">
+          main page
+        </button>
+      </Link>
+    </main>
+  );
+}
